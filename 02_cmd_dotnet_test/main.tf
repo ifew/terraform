eeee67fb3785ca751011df115a890b02209816bb
iFew <chitpong@gmail.com>
2019-02-11 16:29:37 +0700

    Fixed to using interpreter

--- 02_cmd_dotnet_test/main.tf
+++ 02_cmd_dotnet_test/main.tf
@@ -2,7 +2,8 @@ resource "null_resource" "git_pull" {
   provisioner "local-exec" {
     command = "git clone https://github.com/ifew/aws-lambda-function.git project_workspace"
   } 
-    provisioner "local-exec" {
-    command = "/bin/sh -c cd project_workspace/test/aws-lambda-function.Tests && dotnet test"
+  provisioner "local-exec" {
+    command = "cd project_workspace/test/aws-lambda-function.Tests && dotnet test"
+    interpreter = ["/bin/sh", "-c"]
   } 
 }
